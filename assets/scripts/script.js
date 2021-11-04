20f5317a270e4247f9d1af50365195107bfa2a53
satanasova <s.e.atanasowa@gmail.com>
2021-11-04 13:35:00 +0200

    history section - sliced with animation(scrollmagic)
    removed jquery-parallax(used bg on ::before instead)

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,2 +1,19 @@
 import '../../node_modules/jquery/dist/jquery.min.js';
-import '../../node_modules/jquery-parallax.js/parallax.min.js';
+
+$('.history-row').each(function(){
+    $(this).addClass('currentElement');
+    $(this).find('.history-date').addClass('currentElement');
+
+    const controller = new ScrollMagic.Controller();
+
+    const scene = new ScrollMagic.Scene({
+        triggerElement: this,
+        duration: 0,
+        triggerHook: 2
+    })
+    .setClassToggle('.currentElement', 'show')
+    .addTo(controller)
+
+    $(this).removeClass('currentElement','show');
+    $(this).find('.history-date').removeClass('currentElement','show');
+})
